debba7d8f05c2b31679baced7bbf3b68d652db27
zmh0007 <90799188+zmh0007@users.noreply.github.com>
2023-09-28 22:17:52 -0400

    Added Rudimentary Page Routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App/App';
-import reportWebVitals from './reportWebVitals';
+import App from './Components/App';
+import CourseProjects from './Pages/Course Projects/CourseProjects';
+import GameProjects from './Pages/Game Projects/GameProjects';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "course-projects",
+    element: <CourseProjects/>,
+  },
+  {
+    path: "game-projects",
+    element: <GameProjects/>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
