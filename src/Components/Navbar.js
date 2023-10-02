38bc981262980cf3eaa47ad11223bda64b155480
zmh0007 <90799188+zmh0007@users.noreply.github.com>
2023-10-01 20:25:45 -0400

    Fixed Navbar Links

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,17 +7,17 @@ import { Link } from 'react-router-dom';
 
 function NavbarComponent() {
   return (
-    <Navbar expand="lg" className="navbar">
+    <Navbar bg="dark" data-bs-theme="dark" className="navbar">
       <Container className="navbar-content">
         <text className="NavbarTitle">Zach Hubbard</text>
         <Navbar.Toggle classNamearia-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse id="basic-navbar-nav" className='navbar-dropdown'>
           <Nav className="me-auto">
             <Nav.Link><Link className='link' to="/">Home</Link></Nav.Link>
-            <Nav.Link><Link className='link' to="/game-projects">Game Projects</Link></Nav.Link>
-            <Nav.Link><Link className='link' to="/course-projects">Course Projects</Link></Nav.Link>
-            <Nav.Link><Link className='link' to="https://docs.google.com/document/d/1H1XwJ3p6x9V9Zm2vF0q4r6uQJXy5Yb1j/edit?usp=sharing&ouid=116077486025085684967&rtpof=true&sd=true" target="_blank">Resume</Link></Nav.Link>
-            <Nav.Link><Link className='link' to="https://www.linkedin.com/in/zachary-hubbard-39645b204/" target="_blank">LinkedIn</Link></Nav.Link>
+            <Nav.Link className='link' href="/game-projects">Game Projects</Nav.Link>
+            <Nav.Link className='link' href="/course-projects">Course Projects</Nav.Link>
+            <Nav.Link className='link' href="https://drive.google.com/file/d/1OCzhkLCWfZ5nWLExsAy0WAApGkR03BHa/view?usp=sharing">Resume</Nav.Link>
+            <Nav.Link className='link' href="https://www.linkedin.com/in/zachary-hubbard-39645b204/">LinkedIn</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
