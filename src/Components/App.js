bad189541a7133b6da0171cb9f3e4f632eee34ef
zmh0007 <90799188+zmh0007@users.noreply.github.com>
2023-10-16 17:25:21 -0400

    Update to App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -9,6 +9,11 @@ import PortfolioImg from '../Images/PortfolioCropped.jpg';
 import GDCImg from '../Images/GDC.JPG';
 import siteTheme from '../SiteTheme.js';
 import { ThemeProvider } from "@mui/material/styles";
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
 
 
 function App() {
