6c3a50b6ae069dd6317004c5531239d835c58185
zmh0007 <zhubbs19@gmail.com>
2023-10-18 14:23:40 -0400

    Added Seed-A-Pult Demo Video and Itch.io link

--- src/Pages/Games/SeedAPult.js
+++ src/Pages/Games/SeedAPult.js
@@ -1,7 +1,7 @@
 import React from "react";
 import NavbarComponent from "../../Components/Navbar/Navbar";
 import Footer from '../../Components/Footer/Footer.js';
-// import VideoEmbed from '../../Components/EmbeddedVideo/EmbeddedVideo.js';
+import VideoEmbed from '../../Components/EmbeddedVideo/EmbeddedVideo.js';
 import { Grid } from "@mui/material"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../Components/App.css';
@@ -15,20 +15,23 @@ function SeedAPult() {
       <h1 className="base-header">Appalachian Seed-A-Pult</h1>
       <div className="about-content">
         <Grid container direction="row" justifyContent="center" className='about-grid'>
-          {/* <Grid item className='grid-item'>
-            <VideoEmbed videoURL="https://drive.google.com/uc?id=1Bk-RdEVw2AvhL1ETO0B1dZNKIeOdyaZE"></VideoEmbed>
-          </Grid> */}
           <Grid item className='info-item'>
-            <p>Appalachian Seed-A-Pult is a 2D WebGL seed-launching game where you are tasked with launching a seed to the correct location where it is fertile and can grow. 
-            This game was created in the Unity Game Engine through Mon River Games&apos; 
-            participation in the 2023 Global Game Jam, with 4 members participating over 2 days.
+            <VideoEmbed videoURL="https://drive.google.com/uc?id=126AtST8CIzQISODfxTkthe8DrFR7xvrU"></VideoEmbed>
+          </Grid>
+          <Grid item className='info-item'>
+            <p>Appalachian Seed-A-Pult is a 2D WebGL seed-launching game where you are tasked with catapulting a seed to the correct type of soil where it is fertile and is able grow. 
+            This game was created in the Unity Game Engine through Mon River Games&apos; participation in the 2023 Global Game Jam, with 4 members participating over 2 days. 
+            The theme of the game jam was &quot;Roots&quot;, and we decided to create a game that used both of its definitions, as the player is both rooting a seed 
+            while in the backdrop of three notable locations of West Virginia for each of its three levels, where Mon River Games has its roots, namely Coopers Rock, New River Gorge,
+            and Douglas Falls. 
+            <div className="list"></div>
               <h3 className="subheader"> Throughout the Game&apos;s development, I implemented:</h3>
               <li className='feature-list'>Collision detection for seeds colliding on fertile/infertile ground 
               and triggering plant growth</li>
               <li className='feature-list'>Win and Fail Screens</li>
               <li className='feature-list'>Tweaks to seed launch physics</li>
               <li className='feature-list'>Audio and Asset imports</li>
-              <h3 className="subheader"><a href="https://gdimwvu.itch.io/appalachian-seed-a-pult">Itch.io Page</a></h3></p>
+              <iframe src="https://itch.io/embed/1911063" width="552" height="167"><a href="https://gdimwvu.itch.io/appalachian-seed-a-pult">Appalachian Seed-A-Pult by GDIM</a></iframe></p>
           </Grid>
         </Grid>
       </div>
