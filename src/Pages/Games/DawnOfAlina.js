9d4411e78bcef802a682c7d934fcc5d3fcfbb0c9
zmh0007 <zhubbs19@gmail.com>
2024-03-28 23:24:07 -0400

    Updated Attribution Info for Dawn of Alina

--- src/Pages/Games/DawnOfAlina.js
+++ src/Pages/Games/DawnOfAlina.js
@@ -21,8 +21,8 @@ function DawnOfAlina() {
         </Grid>
         <Grid item className='info-item' xs={12} sm={12} md={12} lg={12} xl={6}>
         <p> Dawn of Alina was a 3D action-adventure game prototype. The game was centered around main character Alina, who was tasked with ridding her world of the 
-        evil that inhabits it using her magical powers. The game was created in Unity as part of Mon River Games&apos; summer 2022 session.</p>
-        Development was discontinued following the end of the Fall 2022 session.
+        evil that inhabits it using her magical powers. The game was created in Unity as part of Mon River Games&apos; summer 2022 session. Development was discontinued following the end of the Fall 2022 session.</p>
+        <a style={{fontWeight:"bold"}} href="https://www.monrivergames.com/dawnofalina">Mon River Games - Dawn of Alina Page</a>
             <h3 className="subheader" style={{paddingTop:"2rem"}}> Throughout the game&apos;s development, I implemented:</h3>
             <ul className="feature-list">
               <li className="feature">A working inventory system using item scriptable objects, containing item pickup mechanics, item stacking/removal, and item info displays.</li>
@@ -33,6 +33,14 @@ function DawnOfAlina() {
               <li className='feature'>Cauldron crafting system.</li>
               <li className="sublist">Alina can craft potions by interacting with a cauldron.</li>
             </ul>
+          <p>To develop this system, I viewed tutorials such as Coding with Unity Inventory System Tutorial Series 
+            and Brackey&apos;s Making an RPG Series episodes in order to conceptualize and plan out the 
+            code for the system I implemented, particularly for helping to make my inventory update when items 
+            were added/removed from the inventory. Some functionality and function names were 
+            used from these and as such have been cited within the code.</p>
+            <h3 className="attribution-header" style={{justifyContent:"left", paddingBottom:"0.5rem", paddingTop:"1rem"}}>Tutorial Playlists Referenced:</h3>
+            <li className='attribution-list'><a href="https://youtube.com/playlist?list=PLPV2KyIb3jR4KLGCCAciWQ5qHudKtYeP7&si=e-dJQ52ikEBgA5TG">How to make an RPG in Unity Playlist by Brackeys on YouTube</a></li>
+            <li className='attribution-list'><a href="https://youtube.com/playlist?list=PLJWSdH2kAe_Ij7d7ZFR2NIW8QCJE74CyT&si=HPn-h_TqZiedZOqY">Unity3D - Inventory System w/ Scriptable Objects Playlist by Coding With Unity on YouTube</a></li>
         </Grid>
       </Grid>
     </div>
--- src/Pages/Games/WVQuest.js
+++ src/Pages/Games/WVQuest.js
@@ -28,13 +28,17 @@ function WVQuest() { // Handles the project page for WV Quest App
                 exhibits, prompting the user with a minigame based on the exhibit</p>
                 <p>This project was developed in Unity and uses a Firebase realtime database
                 to store user data. The project was worked on in a seven-person group
-                as part of my West Virginia University Senior Capstone project.</p>
-                <h3 className="subheader"> Throughout continuing the app&apos;s development, I implemented:</h3>
-                <li className='feature'>Unity and Firebase Integration</li>
-                <li className='feature'>User/Group Registration and Login</li>
-                <li className='feature'>Creation of a Switchboard Minigame Prototype</li>
-                <li className='feature'>Leaderboard System</li>
-                <li className='feature'>UpdatedQR scanning system.</li>
+                as part of my West Virginia University Senior Capstone project, 
+                continuing the project from the previous years&apos; group.</p>
+                <a style={{fontWeight:"bold"}} href="https://www.monrivergames.com/wvquest">Mon River Games - WV Quest Page</a>
+                <h3 className="subheader"> Throughout the app&apos;s development, I have implemented:</h3>
+                <li className='feature'>Unity and Firebase integration</li>
+                <li className='feature'>User/Group registration and login</li>
+                <li className='feature'>Creation of a Switchboard minigame prototype</li>
+                <li className='feature'>A leaderboard displaying the top 8 players&apos; scores across minigames.</li>
+                <li className='feature'>Improved QR code scanning system.</li>
+                <li className='feature'>System for efficently loading/unloading UI elements.</li>
+                <li className='feature'>User persistence mechanics.</li>
           </Grid>
           </Grid>
       </div>
